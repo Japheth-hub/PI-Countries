b6c768adb77e39b7c04c09a9f0956a5cabeae72d
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-08 16:08:38 -0600

    Act

--- client/src/helpers/endPoints.js
+++ client/src/helpers/endPoints.js
@@ -1,7 +1,7 @@
-// export const URLcountries = 'http://localhost:3001/countries'
-// export const URLactivities = 'http://localhost:3001/activities'
-// export const URLrelaciones = 'http://localhost:3001/relacion'
+export const URLcountries = 'http://localhost:3001/countries'
+export const URLactivities = 'http://localhost:3001/activities'
+export const URLrelaciones = 'http://localhost:3001/relacion'
 
-export const URLcountries = 'https://backcountries.vercel.app/countries'
-export const URLactivities = 'https://backcountries.vercel.app/activities'
-export const URLrelaciones = 'https://backcountries.vercel.app/relacion'
+// export const URLcountries = 'https://backcountries.vercel.app/countries'
+// export const URLactivities = 'https://backcountries.vercel.app/activities'
+// export const URLrelaciones = 'https://backcountries.vercel.app/relacion'
