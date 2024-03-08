df1c7405e8ad616312ec01460618409620064756
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-08 00:13:43 -0600

    Pruebas

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const guardarDatos = require('./src/helpers/guardarDatos')
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 
 
 conn.sync({ force: true })
   .then(() => guardarDatos())
   .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+    server.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
     })
   })
   .catch(error => console.error(error))
