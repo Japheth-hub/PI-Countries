55ce44ee0c6caca63ab0d337fc6b87c570685d18
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-08 17:05:31 -0600

    Actualizado

--- client/src/helpers/endPoints.js
+++ client/src/helpers/endPoints.js
@@ -1,7 +1,7 @@
-export const URLcountries = 'http://localhost:3001/countries'
-export const URLactivities = 'http://localhost:3001/activities'
-export const URLrelaciones = 'http://localhost:3001/relacion'
+// export const URLcountries = 'http://localhost:3001/countries'
+// export const URLactivities = 'http://localhost:3001/activities'
+// export const URLrelaciones = 'http://localhost:3001/relacion'
 
-// export const URLcountries = 'https://backcountries.vercel.app/countries'
-// export const URLactivities = 'https://backcountries.vercel.app/activities'
-// export const URLrelaciones = 'https://backcountries.vercel.app/relacion'
+export const URLcountries = 'https://pi-countries-x4bv.onrender.com/countries'
+export const URLactivities = 'https://pi-countries-x4bv.onrender.com/activities'
+export const URLrelaciones = 'https://pi-countries-x4bv.onrender.com/relacion'
