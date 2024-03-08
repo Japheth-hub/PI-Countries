cf86b6a21b7d30e796649088274f28bcce12a27c
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-08 16:37:34 -0600

    Act

--- server/src/db.js
+++ server/src/db.js
@@ -5,6 +5,12 @@ require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
   native: false,
 });
