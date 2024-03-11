739ba0b883be36a6459c4d40c56840ecf46a07d4
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-11 12:52:53 -0600

    Actualizado a force: true

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const guardarDatos = require('./src/helpers/guardarDatos')
 const port = process.env.PORT;
 
 
-conn.sync({ force: false })
+conn.sync({ force: true })
   .then(() => guardarDatos())
   .then(() => {
     server.listen(port, () => {
